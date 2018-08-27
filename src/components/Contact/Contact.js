e777abc29b2109eed137020a0bef530c914bb232
aliceviera <39549497+aliceviera@users.noreply.github.com>
2018-08-27 14:55:38 -0400

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-// import '../../styles/Contact.css';
+import React, { Component } from "react";
+import "../../styles/Contact.css";
 
-// import Header from './Header.js';
-// import Chat from './Chat.js';
-// import Help from './Help.js';
-// import SocialMediaIcons from './SocialMediaIcons.js'
-// import Follow from './Follow.js'
-// import Copyright from './Copyright.js'
+// Components
+import Header from "./Header.js";
+import Chat from "./Chat.js";
+import Help from "./Help.js";
+import SocialMediaIcons from "./SocialMediaIcons.js";
+import Follow from "./Follow.js";
+import Copyright from "./Copyright.js";
 
 class Contact extends Component {
   render() {
     return (
       <div className="Contact" id="Contact">
-      // < Header />
-      // < Chat />
-      // < Help />
-      // < SocialMediaIcons />
-      // < Follow /> 
-      // < Copyright />
+        <Header />
+        <Chat />
+        <Help />
+        <SocialMediaIcons />
+        <Follow />
+        <Copyright />
       </div>
     );
   }
