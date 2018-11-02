4c32b54f0a8a61fd3e1b71f3f7f092abbbfb3dc3
Linda Lim <CookiesNCream@users.noreply.github.com>
2018-11-02 16:36:57 +0800

    Fix navbar style transition

--- public/script.js
+++ public/script.js
@@ -8,20 +8,32 @@ $(document).ready(function(){
         $('.nav-wrapper').removeClass('solid');
         $('#nav-about').removeClass('solid');
         $('#nav-about-label').removeClass('bold');
+        $('#nav-video').removeClass('solid');
+        $('#nav-video-label').removeClass('bold');
+        $('#nav-whatwedo').removeClass('solid');
+        $('#nav-whatwedo-label').removeClass('bold');
         $('#nav-contact').removeClass('solid');
         $('#nav-contact-label').removeClass('bold');
       } else if ($(this).scrollTop() >= $('.Home').height() - 10 &&
-                 $(this).scrollTop() < $('.Home').height() + $('.About').height())
+                 $(this).scrollTop() < $('.Home').height() + $('.About').height() + $('.Video').height() + $('.WhatWeDo').height())
       {
         $('.nav-wrapper').addClass('solid');
         $('#nav-about').addClass('solid');
         $('#nav-about-label').addClass('bold');
+        $('#nav-video').addClass('solid');
+        $('#nav-video-label').addClass('bold');
+        $('#nav-whatwedo').addClass('solid');
+        $('#nav-whatwedo-label').addClass('bold');
         $('#nav-contact').removeClass('solid');
         $('#nav-contact-label').removeClass('bold');
       } else {
         $('.nav-wrapper').addClass('solid');
         $('#nav-about').removeClass('solid');
         $('#nav-about-label').removeClass('bold');
+        $('#nav-video').removeClass('solid');
+        $('#nav-video-label').removeClass('bold');
+        $('#nav-whatwedo').removeClass('solid');
+        $('#nav-whatwedo-label').removeClass('bold');
         $('#nav-contact').addClass('solid');
         $('#nav-contact-label').addClass('bold');
       }
